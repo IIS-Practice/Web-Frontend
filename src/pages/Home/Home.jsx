ba8a6aa2ce6d6aa06ccde596d3f3c1e63c9c3db8
aligenzzz <alina_bybko@mail.ru>
2024-06-16 20:01:28 +0300

    feat: add employment page, scrum/waterfall modal, modal form, bugfixes, change css to scss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.styles.css";
+import OutlinedButton from "@components/shared/OutlinedButton";
+import WaterfallModal from "./components/WaterfallModal";
+import ScrumModal from "./components/ScrumModal";
 
 const Home = () => {
-  return <h1>HOME PAGE</h1>;
+  const [showWaterfallModal, setShowWaterfallModal] = useState(false);
+  const openWaterfallModal = () => setShowWaterfallModal(true);
+  const closeWaterfallModal = () => setShowWaterfallModal(false);
+
+  const [showScrumModal, setShowScrumModal] = useState(false);
+  const openScrumModal = () => setShowScrumModal(true);
+  const closeScrumModal = () => setShowScrumModal(false);
+
+  return (
+    <div>
+      <OutlinedButton text="WATERFALL" onClick={openWaterfallModal} />
+      <WaterfallModal
+        showModal={showWaterfallModal}
+        closeModal={closeWaterfallModal}
+      />
+
+      <OutlinedButton text="SCRUM" onClick={openScrumModal} />
+      <ScrumModal showModal={showScrumModal} closeModal={closeScrumModal} />
+    </div>
+  );
 };
 
 export default Home;
