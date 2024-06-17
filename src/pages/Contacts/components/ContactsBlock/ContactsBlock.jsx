149aeb344b6124ef2de572bea770f0738870db35
Chaisy <dashulianorton@gmail.com>
2024-06-17 22:05:04 +0300

    feat: fix icons and positions of text and links

--- src/pages/Contacts/components/ContactsBlock/ContactsBlock.jsx
+++ src/pages/Contacts/components/ContactsBlock/ContactsBlock.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import "./ContactsBlock.styles.css";
+import "./ContactsBlock.styles.css"
 
-const ContactsBlock = ({ picture, name, text, data }) => {
+const ContactsBlock = ({ picture, name, text,link, data }) => {
   return (
+
     <div className="contacts-block">
-      <div>
-        <img className="picture-image" src={picture} alt="pic" />
-      </div>
+      <div>{picture}</div>
       <h2 className="name">{name}</h2>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <p className="text">{text}</p>
       </div>
 
-      <p className="data">{data}</p>
+      <p className="data">
+        <a  href={link}>{data}</a>
+      </p>
+
     </div>
   );
 };
