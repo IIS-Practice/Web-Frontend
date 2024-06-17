09c18a71229b9c93f77ceb1875f6f3b37304ed63
aligenzzz <alina_bybko@mail.ru>
2024-06-17 22:45:13 +0300

    fix: codestyle

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -11,22 +11,22 @@ const Contacts = () => {
     {
       picture: <PhoneIcon />,
       name: "Телефон",
-      text:"Наши специалисты готовы принимать Ваши звонки в рабочее время офиса с 9:00 до 17:00 с понедельника по пятницу.",
-      link:"tel:+375296764510",
+      text: "Наши специалисты готовы принимать Ваши звонки в рабочее время офиса с 9:00 до 17:00 с понедельника по пятницу.",
+      link: "tel:+375296764510",
       data: "+375 (29) 676-45-10",
     },
     {
-      picture:<LetterIcon />,
+      picture: <LetterIcon />,
       name: "Электронная почта",
-      text:"Заявки на электронную почту мы принимаем ежедневно в режиме 24/7. Будьте уверены, мы обязательно с вами свяжемся!",
-      link:"mailto:contact@example.com",
+      text: "Заявки на электронную почту мы принимаем ежедневно в режиме 24/7. Будьте уверены, мы обязательно с вами свяжемся!",
+      link: "mailto:contact@example.com",
       data: "contact@example.com",
     },
     {
-      picture: <LocationIcon/>,
+      picture: <LocationIcon />,
       name: "Местоположение офиса",
-      text:"Приглашаем Вас в наш офис. Также, есть возможность встреч с партнерами и заказчиками в удобном для Вас месте.",
-      link:"https://yandex.by/maps/157/minsk/house/Zk4YcwJnSUIHQFtpfXVwd3prYA==/?ll=27.557070%2C53.916685&z=17.15",
+      text: "Приглашаем Вас в наш офис. Также, есть возможность встреч с партнерами и заказчиками в удобном для Вас месте.",
+      link: "https://yandex.by/maps/157/minsk/house/Zk4YcwJnSUIHQFtpfXVwd3prYA==/?ll=27.557070%2C53.916685&z=17.15",
       data: "г. Минск, ул. Кропоткина, д.44",
     },
   ];
@@ -36,25 +36,29 @@ const Contacts = () => {
       <h1 className="contact-head">Контакты</h1>
       <div className="contacts-blocks">
         {contactsData.map((contact, index) => (
-          <ContactsBlock class="contact-element"
-                         picture = {contact.picture}
-                         name = {contact.name}
-                         text = {contact.text}
-                         link={contact.link}
-                         data={contact.data}
+          <ContactsBlock
+            className="contact-element"
+            key={index}
+            picture={contact.picture}
+            name={contact.name}
+            text={contact.text}
+            link={contact.link}
+            data={contact.data}
           />
         ))}
       </div>
 
       <div>
-        <h1 className="contact-head">Мы рады увидеть <span className="form-head">Вашу</span> заявку!</h1>
-        <p className="contact-p">Пожалуйста оставьте Ваши имя и телефон, мы будем счастливы помочь Вам.</p>
+        <h1 className="contact-head">
+          Мы рады увидеть <span className="form-head">Вашу</span> заявку!
+        </h1>
+        <p className="contact-p">
+          Пожалуйста оставьте Ваши имя и телефон, мы будем счастливы помочь Вам.
+        </p>
         <div className="contact-form">
           <Form />
         </div>
       </div>
-
-
     </div>
   );
 };
--- src/pages/Contacts/components/ContactsBlock/ContactsBlock.jsx
+++ src/pages/Contacts/components/ContactsBlock/ContactsBlock.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import "./ContactsBlock.styles.css"
+import "./ContactsBlock.styles.css";
 
-const ContactsBlock = ({ picture, name, text,link, data }) => {
+const ContactsBlock = ({ picture, name, text, link, data }) => {
   return (
-
     <div className="contacts-block">
       <div>{picture}</div>
       <h2 className="name">{name}</h2>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <p className="text">{text}</p>
       </div>
 
       <p className="data">
-        <a  href={link}>{data}</a>
+        <a href={link}>{data}</a>
       </p>
-
     </div>
   );
 };
