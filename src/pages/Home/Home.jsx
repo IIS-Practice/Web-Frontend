d1e9f77d1d03fd649d2dff161d527328cdd8b7eb
Arseni20032 <teplyakovarseni@mail.ru>
2024-06-18 22:00:48 +0300

    feat: add projects block

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.styles.css";
 import HomeMainBlock from "./components/MainBlock/HomeMainBlock";
+import HomeProjects from "./components/MainBlock/Projects/HomeProjects";
 
 const Home = () => {
   return (
     <div>
        <HomeMainBlock/>
+       <HomeProjects/>
     </div>
   )
 };
