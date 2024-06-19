9badc518bc5a9ca4b353c5640377db1bc4533f42
aligenzzz <alina_bybko@mail.ru>
2024-06-19 14:14:32 +0300

    fix: modals height design + form connection

--- src/pages/Home/components/Contacts/HomeContacts.jsx
+++ src/pages/Home/components/Contacts/HomeContacts.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './HomeContacts.styles.css';
+import ModalForm from "@components/shared/ModalForm";
 
 const HomeContacts = () => {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
   return (
     <div className="home-contact">
       <h3>Давайте приступим к работе?</h3>
-      <Link to="/contacts" className="contact-link">Связаться с нами</Link>
+      <button className="contact-link" text="Оставить&nbsp;заявку" onClick={openModal}>Связаться с нами</button>
+      <ModalForm showModal={showModal} closeModal={closeModal} />
       <hr></hr>
     </div>
   );
