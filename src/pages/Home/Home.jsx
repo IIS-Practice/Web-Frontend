2d0e5a71d317de70e06b920c6fa11358d26c4b50
aligenzzz <alina_bybko@mail.ru>
2024-06-19 17:52:58 +0300

    fix: resolve conflicts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Home.styles.css";
-import HomeTasks from "./components/Tasks/HomeTasks";
-import HomeContacts from "./components/Contacts/HomeContacts";
-import HomeProjects from "./components/Projects/HomeProjects";
-import HomeMainBlock from "./components/MainBlock/HomeMainBlock";
+import {
+  HomeTasks,
+  HomeContacts,
+  HomeProjects,
+  HomeMainBlock,
+} from "./components";
 
 const Home = () => {
   return (
