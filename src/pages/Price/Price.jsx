a7dfe9e1a27c14cb4d17bb1d82d36fe31e4b8c06
ValentinKrasovski <92800455+ValentinKrasovski@users.noreply.github.com>
2024-06-21 23:36:36 +0300

    Feat: made skelet for price page

--- src/pages/Price/Price.jsx
+++ src/pages/Price/Price.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./Price.styles.css";
 
+const card = [
+  {
+    title: "Разработка сайта",
+    price: "от 1000 $",
+    features: [
+      "Составление ТЗ",
+      "Дизайн интерфейса",
+      "Техническая разработка",
+      "Тестирование"
+    ]
+  },
+  {
+    title: "Разработка мобильного приложения",
+    price: "от 1000 $",
+    features: [
+      "Составление ТЗ",
+      "Дизайн интерфейса",
+      "Техническая разработка",
+      "Тестирование"
+    ]
+  },
+  {
+    title: "Разработка бота",
+    price: "от 1000 $",
+    features: [
+      "Составление ТЗ",
+      "Разработка",
+      "Интеграция",
+      "Тестирование"
+    ],
+  },
+  {
+    title: "UX/UI Дизайн",
+    price: "от 1000 $",
+    features: [
+      "Составление ТЗ",
+      "Прототип",
+      "Дизайн интерфейса"
+    ],
+  }
+]
+
 const Price = () => {
-  return <h1>PRICE PAGE</h1>;
+  return (<main>
+    <h1>Прайс-лист</h1>
+    <div className="priceGridContainer">
+    {card?.map((description) => 
+      <div>
+        <h2>{description?.title}</h2>
+        <h2>{description?.price}</h2>
+        {description?.features?.map(feature => <div>{feature}</div>)}
+      </div>
+    )}
+    </div>
+  </main>);
 };
 
 export default Price;
