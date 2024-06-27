187d3965c6ae14bb9be1684e49ef6b9d6f58b60c
Arseni20032 <teplyakovarseni@mail.ru>
2024-06-27 15:53:44 +0300

    feat: fix adaptive bugs

--- src/pages/Home/components/MainBlock/HomeMainBlock.jsx
+++ src/pages/Home/components/MainBlock/HomeMainBlock.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomeMainBlock.styles.css";
 import CompanyImage from "@assets/CompanyImage.png";
+import OutlinedButton from "@components/shared/OutlinedButton";
+
 
 const HomeMainBlock = () => {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+
   return (
     <div className="MainBlock">
       <div className="CompanyName">
         <div className="text-container">
           <h1>IISolutions -</h1>
           <h2>ваши идеи, наше</h2>
-          <h2> воплощение</h2>
+          <h2>воплощение</h2>
+          <OutlinedButton text="Оставить&nbsp;заявку" onClick={openModal} />
         </div>
       </div>
       <div className="CompanyImage">
